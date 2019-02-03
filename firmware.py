d656aea2d5b3be9bdfc66c95853932a91620a6b6
Ricky Yuan <ricky.yuan@outlook.com>
2019-02-03 14:12:33 +0100

    Add firmware

---
+++ firmware.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+import ev3dev.ev3 as ev3
+import random
+import time
+
+class GrowBot:
+    # Static variables
+    ## Steering speeds
+    small_steer_speed = 100
+    medium_steer_speed = 100
+    large_steer_speed = 100
+
+    ## Steer time, in milliseconds
+    small_steer_time = 50
+    medium_steer_time = 100
+    large_steer_time = 150
+    
+    ## Drive speeds
+    forward_speed = 500
+
+    # Initializer / Instance Attributes
+    def __init__(self, battery_level, water_level):
+        self.battery_level = battery_level
+        self.water_level = water_level
+
+        ## Definitions of all sensors and motors
+        self.driving_motor = ev3.LargeMotor('outA')
+        self.steering_motor = ev3.MediumMotor('outB')
+        self.obstacle_sensor = ev3.UltrasonicSensor('in1')
+
+        ## Detect all ports are connected
+        ## Expansion: detect types of each port?
+        if not (self.driving_motor.connected):
+            raise IOError("Plug the large motor into Port A.")
+        elif not (self.steering_motor.connected):
+            raise IOError("Plug the small motor into Port B.")
+        elif not (self.obstacle_sensor.connected):
+            raise IOError("Plug the small motor into Port 1.")
+
+    def smallLeft(self):
+        # Function will move front wheels left by small amount for ~1sec
+        # Precond: driving motor not idle
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = -GrowBot.small_steer_speed, time_sp = GrowBot.small_steer_time)
+
+    def mediumLeft(self):
+        # Function will move front wheels left by medium amount for ~1.5sec
+        # Precond: driving motor not idle
+        (self.driving_motor.is_running)
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = -GrowBot.medium_steer_speed, time_sp = 375)
+
+
+    def largeLeft(self):
+        # Function will move front wheels left by large amount for ~2sec
+        # Precond: driving motor not idle
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = -GrowBot.large_steer_speed, time_sp = GrowBot.large_steer_time)
+
+    def smallRight(self):
+        # Function will move front wheels right by small amount for ~1sec
+        # Precond: driving motor not idle
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = GrowBot.small_steer_speed, time_sp = GrowBot.small_steer_time)
+    
+    def mediumRight(self):
+        # Function will move front wheels right by medium amount for ~1.5sec
+        # Precond: driving motor not idle
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = GrowBot.medium_steer_speed, time_sp = 375)
+
+    def largeRight(self):
+        # Function will move front wheels right by small amount for ~2sec
+        # Precond: driving motor not idle
+        if (self.driving_motor.is_running):
+            self.steering_motor.run_timed(speed_sp = GrowBot.large_steer_speed, time_sp = GrowBot.large_steer_time)
+
+    def stop(self):
+        # Function will set the driving motor to idle
+        self.driving_motor.stop(stop_action="brake")
+
+    def forward(self, speed=500):
+        # Function will set the driving motor to have +tive speed specified
+        # Precond speed > 0
+        if (speed <= 0):
+            raise (ValueError("Speed must be positive"))
+        else:
+            self.driving_motor.run_forever(speed_sp = speed)
+
+    def reverse(self, speed=500):
+        # Function will set the driving motor to have -tive speed specified
+        # Precond speed > 0
+        if (speed <= 0):
+            raise (ValueError("Speed must be positive"))
+        else:
+            self.driving_motor.run_forever(speed_sp = -speed)
+
+    def backUp(self):
+        # Function call will tell the robot to back up around 1m at a reasonable speed
+        return
+
+    # def pumpWater(self, time):
+    #     #Function will set the pumpung motor into action for the specified period of time
+    #     # precond time>0
+    #     return
+
+    # def pumpWater(self):
+    #     #Function will set the pumpung motor into action for the set period of time
+    #     return
+
+    # PLUS MAY MORE PROBABLY...
+
+
+# Create a main method which makes the robot move around randomly. This will be very useful for training the vision system.
+def run_forever(growbot):
+    while (True):
+        if (random.random() < 0.25):
+            growbot.stop()
+        else:
+            drive_rand = random.random()
+            steer_rand = random.random()
+            intensity_rand = random.random()
+
+            # Driving randomiser
+            if (drive_rand < 0.5):
+                if (random.random() < 0.8):
+                    growbot.forward()
+                else:
+                    growbot.forward(speed=random.randint(1, 1000))
+            else:
+                if (random.random() < 0.8):
+                    growbot.reverse()
+                else:
+                    growbot.reverse(speed=random.randint(1,1000))
+
+            # Steering randomiser
+            if (steer_rand < 0.5):
+                if (intensity_rand < 0.33):
+                    growbot.smallLeft()
+                elif (intensity_rand < 0.67):
+                    growbot.mediumLeft()
+                else:
+                    growbot.largeLeft()
+            else:
+                if (intensity_rand < 0.33):
+                    growbot.smallRight()
+                elif (intensity_rand < 0.67):
+                    growbot.mediumRight()
+                else:
+                    growbot.largeRight()
+
+        # Take a break until the next command
+        time.sleep(2)
+
+def main():
+    grow_bot_inst = GrowBot(-1, -1) # No parameters yet
+    run_forever(grow_bot_inst)
+
+if __name__ == "__main__":
+    main()
