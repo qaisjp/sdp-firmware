77186e486b9c0a89148acae9875c50bfecba0aaa
Qais Patankar <qaisjp@gmail.com>
2019-03-11 01:14:00 +0000

    Add example to using scheduler

---
+++ scheduler_example.py
@@ -0,0 +1,40 @@
+import asyncio
+from scheduler import Scheduler, Event, Action, ActionName
+from dateutil.rrule import rrule, rruleset, SECONDLY
+from datetime import datetime
+
+
+def check_24_hours():
+    s = Scheduler()
+    asyncio.ensure_future(s.run())
+
+    # EXAMPLE DOWNLOAD START
+    rule1 = rrule(freq=SECONDLY, interval=5, dtstart=datetime.now(), count=60)
+    rule2 = rrule(freq=SECONDLY, interval=9, dtstart=datetime.now(), count=60)
+
+    a1 = Action(ActionName.PLANT_CAPTURE_PHOTO, 5)
+    a2 = Action(ActionName.PLANT_WATER, 9)
+
+    e1 = Event()
+    e1.recurrences = [str(rule1)]
+    e1.actions = [a1]
+
+    e2 = Event()
+    e2.recurrences = [str(rule2)]
+    e2.actions = [a2]
+    # EXAMPLE DOWNLOAD END
+
+    s.push_events([e1, e2])
+
+
+def run():
+    check_24_hours()
+
+    loop = asyncio.get_event_loop()
+    pending = asyncio.Task.all_tasks()
+    loop.run_until_complete(asyncio.gather(*pending))
+
+
+if __name__ == "__main__":
+    run()
+    print("Completed!")
