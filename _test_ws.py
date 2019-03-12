788d3d0484d2f58f63631200c5e943c18ad311a3
Qais Patankar <qaisjp@gmail.com>
2019-03-12 19:11:05 +0000

    _test_ws: fix lint

--- _test_ws.py
+++ _test_ws.py
@@ -1,21 +1,23 @@
 from RemoteMotorController import RemoteMotorController
-from time import sleep
 import asyncio
 import threading
+
 
 def sender_action(rm, loop):
     asyncio.set_event_loop(loop)
     rm.connect()
     loop.run_forever()
 
+
 def receiver_action(rm, loop):
     asyncio.set_event_loop(loop)
     rm.connect(sender=False, port_nr=19221)
     loop.run_forever()
 
+
 def main():
     rm = RemoteMotorController()
-    
+
     ws_sender_loop = asyncio.new_event_loop()
     ws_sender_thread = threading.Thread(target=sender_action, args=(rm, ws_sender_loop,))
     ws_sender_thread.setDaemon(True)
@@ -26,15 +28,15 @@ def main():
     ws_receiver_thread.setDaemon(True)
     ws_receiver_thread.start()
 
-
     wait_loop = asyncio.new_event_loop()
     task = wait_loop.create_task(asyncio.sleep(5))
     wait_loop.run_until_complete(task)
-    
+
     while True:
         rm.turn_left()
 
     asyncio.get_event_loop().run_forever()
+
 
 if __name__ == "__main__":
     main()
