add17540961782f307d7112af92f88a5e25cee6c
ocvlt <s1548787@sms.ed.ac.uk>
2019-04-02 13:40:33 +0100

    RobotController is now a Singleton class

--- RobotController.py
+++ RobotController.py
@@ -16,145 +16,149 @@ import cv2
 
 
 class RobotController:
-    model_xml = '/home/student/ssd300.xml'
-    model_bin = '/home/student/ssd300.bin'
+    instance = None
 
     def __init__(self, net, exec_net):
-        self.vision = Vision(
-                        net,
-                        exec_net,
-                        self,
-                        is_headless=True,
-                        live_stream=False,
-                        confidence_interval=0.5)
+        if not RobotController.instance:
+            RobotController.instance = RobotController.__RobotController(net, exec_net)
 
-        self.received_frame = None
-        self.qr_reader = QRReader()
-        self.last_qr_approached = None
-        self.current_qr_approached = None
-        self.approach_complete = True
-        self.retrying_approach = False
-        self.standby_mode = True
+    class __RobotController:
+        def __init__(self, net, exec_net):
+            self.vision = Vision(
+                            net,
+                            exec_net,
+                            self,
+                            is_headless=True,
+                            live_stream=False,
+                            confidence_interval=0.5)
 
-        if config.RESPOND_TO_API:
-            host = config.API_HOST
-            if config.API_SECURE:
-                host = "wss://"+host
+            self.received_frame = None
+            self.qr_reader = QRReader()
+            self.last_qr_approached = None
+            self.current_qr_approached = None
+            self.approach_complete = True
+            self.retrying_approach = False
+            self.standby_mode = True
+
+            if config.RESPOND_TO_API:
+                host = config.API_HOST
+                if config.API_SECURE:
+                    host = "wss://"+host
+                else:
+                    host = "ws://"+host
+
+                self.remote = Remote(config.UUID, host)
+                self.remote.add_callback(
+                    RPCType.MOVE_IN_DIRECTION, self.remote_move)
+                self.remote.add_callback(
+                    RPCType.EVENTS, self.on_events_received)
+
+                rm_thread = threading.Thread(target=self.thread_remote,
+                                             daemon=True)
+                rm_thread.start()
+                # rm_thread.join()
+
+            # Create the navigation system
+            self.navigator = Navigator(self, verbose=True)
+
+            threading.Thread(target=self.vision.start).start()
+
+        def remote_move(self, direction):
+            self.navigator.remote_move(direction)
+
+        def thread_remote(self):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            self.sched = Scheduler()
+            loop.run_until_complete(self.remote.connect())
+
+        def process_visual_data(self, predictions, frame):
+            """
+            Forwards messages to navigator instance.
+            :param predictions:     List of predictions produced by the VPU
+            :return:
+            """
+            if not self.standby_mode:
+                self.received_frame = frame
+                self.navigator.on_new_frame(predictions)
             else:
-                host = "ws://"+host
+                # Stop immediately? Wait until the jobs to finish to stop?
+                if not self.approach_complete:
+                    pass
+                elif self.retrying_approach:
+                    pass
+                else:
+                    # Any other switches to flip?
+                    # Reset read QR codes
+                    self.current_qr_approached = None
+                    self.last_qr_approached = None
+                    # Stop the motor
+                    self.navigator.remote_motor_controller.stop()
 
-            self.remote = Remote(config.UUID, host)
-            self.remote.add_callback(
-                RPCType.MOVE_IN_DIRECTION, self.remote_move)
-            self.remote.add_callback(
-                RPCType.EVENTS, self.on_events_received)
+        def read_qr_code(self):
+            # Read the QR code
+            tries = 3
+            qr_codes = self.qr_reader.identify(self.received_frame)
+            while tries > 0:
+                if len(qr_codes) == 0:
+                    log.warning("No plant QR found.")
+                    tries -= 1
+                else:
+                    for qr in qr_codes:
+                        self.current_qr_approached = qr
+                        log.info("Plant QR found: {}".format(qr))
+                    break
 
-            rm_thread = threading.Thread(target=self.thread_remote,
-                                         daemon=True)
-            rm_thread.start()
-            # rm_thread.join()
-
-        # Create the navigation system
-        self.navigator = Navigator(self, verbose=True)
-
-        threading.Thread(target=self.vision.start).start()
-
-    def remote_move(self, direction):
-        self.navigator.remote_move(direction)
-
-    def thread_remote(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        self.sched = Scheduler()
-        loop.run_until_complete(self.remote.connect())
-
-    def process_visual_data(self, predictions, frame):
-        """
-        Forwards messages to navigator instance.
-        :param predictions:     List of predictions produced by the VPU
-        :return:
-        """
-        if not self.standby_mode:
-            self.received_frame = frame
-            self.navigator.on_new_frame(predictions)
-        else:
-            # Stop immediately? Wait until the jobs to finish to stop?
-            if not self.approach_complete:
-                pass
-            elif self.retrying_approach:
-                pass
+        def on_plant_found(self):
+            # Send message to initiate approach command, until instructed to continue
+            if self.current_qr_approached is None:
+                log.warning("No QR found, retrying approach")
+                self.retrying_approach = True
+                self.navigator.remote_motor_controller.retry_approach()
             else:
-                # Any other switches to flip?
-                # Reset read QR codes
-                self.current_qr_approached = None
-                self.last_qr_approached = None
-                # Stop the motor
-                self.navigator.remote_motor_controller.stop()
+                # if past == current, do something here
+                self.approach_complete = False
+                self.navigator.remote_motor_controller.approached()
 
-    def read_qr_code(self):
-        # Read the QR code
-        tries = 3
-        qr_codes = self.qr_reader.identify(self.received_frame)
-        while tries > 0:
-            if len(qr_codes) == 0:
-                log.warning("No plant QR found.")
-                tries -= 1
+        def on_approach_complete(self):
+            # Take a picture here
+
+            if self.current_qr_approached is not None:
+                if self.current_qr_approached.startswith("gbpl:"):
+                    self.remote.plant_capture_photo(int(self.current_qr_approached[5:]), base64.b64encode(cv2.imencode(".jpg", self.received_frame)[1]).decode("utf-8"))
             else:
-                for qr in qr_codes:
-                    self.current_qr_approached = qr
-                    log.info("Plant QR found: {}".format(qr))
-                break
+                log.warning("[Pi] No QR code found during this approach, photo will not be sent.")
 
-    def on_plant_found(self):
-        # Send message to initiate approach command, until instructed to continue
-        if self.current_qr_approached is None:
-            log.warning("No QR found, retrying approach")
-            self.retrying_approach = True
-            self.navigator.remote_motor_controller.retry_approach()
-        else:
-            # if past == current, do something here
-            self.approach_complete = False
-            self.navigator.remote_motor_controller.approached()
+            self.last_qr_approached = self.current_qr_approached
+            self.current_qr_approached = None
+            self.navigator.remote_motor_controller.approach_escape()
 
-    def on_approach_complete(self):
-        # Take a picture here
+        def on_approach_escape_complete(self):
+            self.navigator.random_search_mode = True # Flip on the random search
+            self.navigator.remote_motor_controller.random_walk()
 
-        if self.current_qr_approached is not None:
-            if self.current_qr_approached.startswith("gbpl:"):
-                self.remote.plant_capture_photo(int(self.current_qr_approached[5:]), base64.b64encode(cv2.imencode(".jpg", self.received_frame)[1]).decode("utf-8"))
-        else:
-            log.warning("[Pi] No QR code found during this approach, photo will not be sent.")
+            self.approach_complete = True
 
-        self.last_qr_approached = self.current_qr_approached
-        self.current_qr_approached = None
-        self.navigator.remote_motor_controller.approach_escape()
+        def on_retry_complete(self):
+            self.retrying_approach = False
 
-    def on_approach_escape_complete(self):
-        self.navigator.random_search_mode = True # Flip on the random search
-        self.navigator.remote_motor_controller.random_walk()
+        def on_plant_seen(self):
+            pass
 
-        self.approach_complete = True
+        def on_events_received(self, data):
+            self.sched.push_events(list(map(Event.from_dict, data)))
+            pass
 
-    def on_retry_complete(self):
-        self.retrying_approach = False
+        def on_leaving_standby(self):
+            # Start random search
+            self.navigator.random_search_mode = True
+            self.navigator.remote_motor_controller.random_walk()
 
-    def on_plant_seen(self):
-        pass
+            # Turn off standby mode
+            self.standby_mode = False
 
-    def on_events_received(self, data):
-        self.sched.push_events(list(map(Event.from_dict, data)))
-        pass
-
-    def on_leaving_standby(self):
-        # Start random search
-        self.navigator.random_search_mode = True
-        self.navigator.remote_motor_controller.random_walk()
-
-        # Turn off standby mode
-        self.standby_mode = False
-
-    def on_entering_standby(self):
-        self.standby_mode = True
+        def on_entering_standby(self):
+            self.standby_mode = True
 
 def main():
     if os.getenv("http_proxy") is not None:
