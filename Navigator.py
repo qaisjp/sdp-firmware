ac5879e4401152eaa60e95f1771f7a9d134c419e
Ricky Yuan <itvcanterlot@gmail.com>
2019-04-03 01:28:12 +0100

    Small tweak of approach distance

--- Navigator.py
+++ Navigator.py
@@ -306,7 +306,7 @@ class Navigator:
                 sensor_count += 1
                 sensor_sum += i
         
-        if sensor_count > 0 and sensor_sum / sensor_count < 600:
+        if sensor_count > 0 and sensor_sum / sensor_count < 450:
             sensor_flag = True
 
         vision_flag = (self.get_bb_area(plant) / self.frame_area) > self.plant_approach_threshold
